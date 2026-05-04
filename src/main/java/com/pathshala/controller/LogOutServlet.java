87e70e3e66ab357d603582b5f4a204e2477e6546
Nischal Raj Joshi <joshinischal10@gmail.com>
2026-05-04 10:33:35 +0545

    Fixed imported libraries

--- src/main/java/com/pathshala/controller/LogOutServlet.java
+++ src/main/java/com/pathshala/controller/LogOutServlet.java
@@ -1,10 +1,13 @@
 package com.pathshala.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet; 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.pathshala.utils.SessionUtil;
 
@@ -13,35 +16,22 @@ import com.pathshala.utils.SessionUtil;
  */
 @WebServlet(asyncSupported = true, urlPatterns = { "/logout" })
 public class LogOutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public LogOutServlet() {
-        // TODO Auto-generated constructor stub
+        super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		// Used Session's invalidateSession method to logout
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        // Used SessionUtil to invalidate session
         SessionUtil.invalidateSession(request);
 
-        // Redirect the user to the login page or home page after logging out, I have chosen login page option
+        // Redirect the user to the login page
         response.sendRedirect(request.getContextPath() + "/login");
-	}
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
